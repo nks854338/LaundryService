32feae15a7a967e7e50db2849ddc7ee91119e9fd
Nandani <nks854338@gmail.com>
2024-06-16 11:43:35 +0530

    Workrd on Services section's functionlity

--- script.js
+++ script.js
@@ -1,39 +1,7 @@
-let service = [
-  {
-    serviceName: "Dry cleaning",
-    servicePrice: "200",
-    serviceImg: "Laundry.png",
-  },
-  { serviceName: "Ironing", servicePrice: "30", serviceImg: "Laundry.png" },
-  {
-    serviceName: "Stain removal",
-    servicePrice: "550",
-    serviceImg: "Laundry.png",
-  },
-  {
-    serviceName: "Wash and fold",
-    servicePrice: "150",
-    serviceImg: "Laundry.png",
-  },
-  {
-    serviceName: "Dry cleaning",
-    servicePrice: "200",
-    serviceImg: "Laundry.png",
-  },
-  {
-    serviceName: "Wash and iron",
-    servicePrice: "350",
-    serviceImg: "Laundry.png",
-  },
-  {
-    serviceName: "Wedding dress cleaning",
-    servicePrice: "3000",
-    serviceImg: "Laundry.png",
-  },
-];
-
+let userName= document.getElementsByClassName("userName")[0];
+let email= document.getElementsByClassName("email")[0];
+let phoneNo= document.getElementsByClassName("phoneNo")[0];
 let cartItems = [];
-
 let cartBtn1 = document.getElementById("cartBtn1");
 let cartBtn2 = document.getElementById("cartBtn2");
 let cartBtn3 = document.getElementById("cartBtn3");
@@ -49,276 +17,242 @@ let btn5 = true;
 let btn6 = true;
 let btn7 = true;
 
+
+
+let service = [
+  {
+    name: "Dry cleaning",
+    Price: "200",
+  },
+  { name: "Ironing", Price: "30", Img: "Laundry.png" },
+  {
+    name: "Stain removal",
+    Price: "550",
+  },
+  {
+    name: "Wash and fold",
+    Price: "150",
+  },
+  {
+    name: "QuickClean Laundry",
+    Price: "500",
+  },
+  {
+    name: "Wash and iron",
+    Price: "350",
+  },
+  {
+    name: "Wedding dress cleaning",
+    Price: "3000",
+  },
+];
+
+
 function getServices() {
   cartBtn1.addEventListener("click", () => {
     if (btn1 == true) {
       cartBtn1.innerText = "Remove Item";
-      cartItems.push(service[0]);
-      console.log(cartItems);
+      cartItems.push({
+        name: service[0].name,
+        Price: service[0].Price,
+      });
       btn1 = false;
+      renderServices();
     } else {
       cartBtn1.innerText = "Add Item";
-      cartItems.pop(service[0]);
-      console.log(cartItems);
+      for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].name == service[0].name) {
+          cartItems.splice(i, 1);
+        }
+      }
       btn1 = true;
+      renderServices();
     }
   });
 
   cartBtn2.addEventListener("click", () => {
     if (btn2 == true) {
       cartBtn2.innerText = "Remove Item";
-      cartItems.push(service[1]);
-      console.log(cartItems);
-      console.log(cartItems);
+      cartItems.push({
+        name: service[1].name,
+        Price: service[1].Price,
+      });
       btn2 = false;
+      renderServices();
     } else {
       cartBtn2.innerText = "Add Item";
-      cartItems.pop(service[1]);
-      console.log(cartItems);
+      for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].name == service[1].name) {
+          cartItems.splice(i, 1);
+        }
+      }
       btn2 = true;
+      renderServices();
     }
   });
 
   cartBtn3.addEventListener("click", () => {
     if (btn3 == true) {
       cartBtn3.innerText = "Remove Item";
-      cartItems.push(service[2]);
-      console.log(cartItems);
+      cartItems.push({
+        name: service[2].name,
+        Price: service[2].Price,
+      });
       btn3 = false;
+      renderServices();
     } else {
       cartBtn3.innerText = "Add Item";
-      cartItems.pop(service[2]);
-      console.log(cartItems);
+      for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].name == service[2].name) {
+          cartItems.splice(i, 1);
+        }
+      }
       btn3 = true;
+      renderServices();
     }
   });
 
   cartBtn4.addEventListener("click", () => {
     if (btn4 == true) {
       cartBtn4.innerText = "Remove Item";
-      cartItems.push(service[3]);
-      console.log(cartItems);
+      cartItems.push({
+        name: service[3].name,
+        Price: service[3].Price,
+      });
       btn4 = false;
+      renderServices();
     } else {
       cartBtn4.innerText = "Add Item";
-      cartItems.pop(service[3]);
-      console.log(cartItems);
+      for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].name == service[3].name) {
+          cartItems.splice(i, 1);
+        }
+      }
       btn4 = true;
+      renderServices();
     }
+
   });
 
   cartBtn5.addEventListener("click", () => {
     if (btn5 == true) {
       cartBtn5.innerText = "Remove Item";
-      cartItems.push(service[4]);
-      console.log(cartItems);
+      cartItems.push({
+        name: service[4].name,
+        Price: service[4].Price,
+      });
       btn5 = false;
+      renderServices();
     } else {
       cartBtn5.innerText = "Add Item";
-      cartItems.pop(service[4]);
-      console.log(cartItems);
+      for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].name == service[4].name) {
+          cartItems.splice(i, 1);
+        }
+      }
       btn5 = true;
+      renderServices();
     }
   });
 
   cartBtn6.addEventListener("click", () => {
     if (btn6 == true) {
       cartBtn6.innerText = "Remove Item";
-      cartItems.push(service[5]);
-      console.log(cartItems);
+      cartItems.push({
+        name: service[5].name,
+        Price: service[5].Price,
+      });
       btn6 = false;
+      renderServices();
     } else {
       cartBtn6.innerText = "Add Item";
-      cartItems.pop(service[5]);
-      console.log(cartItems);
+      for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].name == service[5].name) {
+          cartItems.splice(i, 1);
+        }
+      }
       btn6 = true;
+      renderServices();
     }
   });
 
   cartBtn7.addEventListener("click", () => {
     if (btn7 == true) {
       cartBtn7.innerText = "Remove Item";
-      cartItems.push(service[6]);
-      console.log(cartItems);
+      cartItems.push({
+        name: service[6].name,
+        Price: service[6].Price,
+      });
       btn7 = false;
+      renderServices();
     } else {
       cartBtn7.innerText = "Add Item";
-      cartItems.pop(service[6]);
-      console.log(cartItems);
+      for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].name == service[6].name) {
+          cartItems.splice(i, 1);
+        }
+      }
       btn7 = true;
+      renderServices();
     }
   });
 }
 
 function renderServices() {
-  let totalAmountValue = document.getElementsByClassName("totalAmountValue");
-  let addedItemTable = document.getElementById("addedItemTable");
+  let totalAmountValue = document.getElementById("totalAmountValue");
+  let addedItemTable = document
+    .getElementById("addedItemTable")
+    .getElementsByTagName("tbody")[0];
+  addedItemTable.innerHTML = "";
   let totalAmount = 0;
-  for(let i=0;i<cartItems.length;i++){
+  for (let i = 0; i < cartItems.length; i++) {
     const row = addedItemTable.insertRow();
     row.insertCell(0).innerText = i + 1;
-    row.insertCell(1).innerText = cartItems.name;
-    row.insertCell(2).innerText = `₹${cartItems.price}`;
-    totalAmount += cartItems.price;
-  };
+    row.insertCell(1).innerText = cartItems[i].name;
+    row.insertCell(2).innerText = `₹${cartItems[i].Price}`;
+    totalAmount += parseInt(cartItems[i].Price);
+  }
 
   totalAmountValue.innerText = `₹${totalAmount}`;
 
   console.log(cartItems);
+  cheakAnyItemInCart()
 }
 
 getServices();
-renderServices();
+cheakAnyItemInCart();
+
+
+function cheakAnyItemInCart(){
+   if(cartItems.length === 0){
+    let addedItemTable = document
+    .getElementById("addedItemTable")
+    .getElementsByTagName("tbody")[0];
+    addedItemTable.innerHTML="<div class=noItem>No Item is present</div>";
+    cartBtn1.innerText = "Add Item";
+    cartBtn2.innerText = "Add Item";
+    cartBtn3.innerText = "Add Item";
+    cartBtn4.innerText = "Add Item";
+    cartBtn5.innerText = "Add Item";
+    cartBtn6.innerText = "Add Item";
+    cartBtn7.innerText = "Add Item";
+
+   }
+}
 
 
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   // Define the services object
-//   const services = {
-//     "Dry cleaning": { price: 200.0 },
-//     Ironing: { price: 30.0 },
-//     "Stain removal": { price: 550.0 },
-//     "Wash and fold": { price: 150.0 },
-//     "Wash and iron": { price: 350.0 },
-//     "Wedding dress cleaning": { price: 3000.0 },
-//   };
+function bookNow() {
+  if (userName.value !== "" && email.value !== "" && phoneNo.value !== "" && cartItems.length > 0) {
+    cartItems = [];
+    renderServices();
+    alert("Items booked successfully!")
+    userName.value="";
+    email.value="";
+    phoneNo.value="";
+  } else {
+    alert("Please fill in all details and add items to the cart!");
+  }
+}
 
-//   let cartbtn1 = document.getElementById("cartBtn1");
-//   let cartbtn2 = document.getElementById("cartBtn2");
-//   let cartbtn3 = document.getElementById("cartBtn3");
-//   let cartbtn4 = document.getElementById("cartBtn4");
-//   let cartbtn5 = document.getElementById("cartBtn5");
-//   let cartbtn6 = document.getElementById("cartBtn6");
-//   let cartbtn7 = document.getElementById("cartBtn7");
-
-//   let addedCart = false;
-
-//   let cart = [];
-//   let addedItemTable = document.getElementById("addedItemTable");
-//   let totalAmountValue = document.querySelector(".totalAmountValue");
-
-//   function updateAddedItem() {
-//     let totalAmount = 0;
-//     cart.forEach((item, index) => {
-//       const row = addedItemTable.insertRow();
-//       row.insertCell(0).innerText = index + 1;
-//       row.insertCell(1).innerText = item.name;
-//       row.insertCell(2).innerText = `₹${item.price}`;
-//       totalAmount += item.price;
-//     });
-
-//     totalAmountValue.innerText = `₹${totalAmount}`;
-
-//     console.log(cart);
-//   }
-
-//   function updateRemovedItem() {
-//     let totalAmount = totalAmountValue.innerText;
-//     cart.forEach((item, index) => {
-//       totalAmount -= item.price;
-//     });
-
-//     totalAmountValue.innerText = `₹${totalAmount}`;
-
-//     console.log(cart);
-//   }
-
-//   cartbtn1.addEventListener("click", function () {
-//     const serviceName =
-//       this.closest("tr").querySelector(".serviceName").innerText;
-//     const service = services[serviceName];
-//     if (service) {
-//       cart.push({ name: serviceName, price: service.price });
-//       updateAddedItem();
-//       document.querySelector(".updateCartToggleBtn").innerHtml =
-//         '<div class="addToCartBtn" id="cartBtn2"> RemoveItem <i class="fa-solid fa-circle-plus"></i></div>';
-//     } else if (service) {
-//       cart.pop({ name: serviceName, price: service.price });
-//       updateRemovedItem();
-//     }
-//   });
-
-//   cartbtn2.addEventListener("click", function () {
-//     const serviceName =
-//       this.closest("tr").querySelector(".serviceName").innerText;
-//     const service = services[serviceName];
-//     if (service) {
-//       cart.push({ name: serviceName, price: service.price });
-//       updateAddedItem();
-//     }
-//     if (service) {
-//       cart.pop({ name: serviceName, price: service.price });
-//       updateRemovedItem();
-//     }
-//   });
-
-//   cartbtn3.addEventListener("click", function () {
-//     const serviceName =
-//       this.closest("tr").querySelector(".serviceName").innerText;
-//     const service = services[serviceName];
-//     if (service) {
-//       cart.push({ name: serviceName, price: service.price });
-//       updateAddedItem();
-//     }
-//     if (service) {
-//       cart.pop({ name: serviceName, price: service.price });
-//       updateRemovedItem();
-//     }
-//   });
-
-//   cartbtn4.addEventListener("click", function () {
-//     const serviceName =
-//       this.closest("tr").querySelector(".serviceName").innerText;
-//     const service = services[serviceName];
-//     if (service) {
-//       cart.push({ name: serviceName, price: service.price });
-//       updateAddedItem();
-//     }
-//     if (service) {
-//       cart.pop({ name: serviceName, price: service.price });
-//       updateRemovedItem();
-//     }
-//   });
-
-//   cartbtn5.addEventListener("click", function () {
-//     const serviceName =
-//       this.closest("tr").querySelector(".serviceName").innerText;
-//     const service = services[serviceName];
-//     if (service) {
-//       cart.push({ name: serviceName, price: service.price });
-//       updateAddedItem();
-//     }
-//     if (service) {
-//       cart.pop({ name: serviceName, price: service.price });
-//       updateRemovedItem();
-//     }
-//   });
-
-//   cartbtn6.addEventListener("click", function () {
-//     const serviceName =
-//       this.closest("tr").querySelector(".serviceName").innerText;
-//     const service = services[serviceName];
-//     if (service) {
-//       cart.push({ name: serviceName, price: service.price });
-//       updateAddedItem();
-//     }
-//     if (service) {
-//       cart.pop({ name: serviceName, price: service.price });
-//       updateRemovedItem();
-//     }
-//   });
-
-//   cartbtn7.addEventListener("click", function () {
-//     const serviceName =
-//       this.closest("tr").querySelector(".serviceName").innerText;
-//     const service = services[serviceName];
-//     if (service) {
-//       cart.push({ name: serviceName, price: service.price });
-//       updateAddedItem();
-//     }
-//     if (service) {
-//       cart.pop({ name: serviceName, price: service.price });
-//       updateRemovedItem();
-//     }
-//   });
-// });
+document.querySelectorAll(".addToCartBtn").innerText="hello";
